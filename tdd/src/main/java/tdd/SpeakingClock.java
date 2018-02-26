e0ed58c05b6d5bd6ab73a3f297bbe9cc8e500799
RealityCtrl <david.mccandless@outlook.com>
2018-02-26 11:58:54 +0000

    updating SpeakingClock getTimeAs to Static and adding constructors plus non-static method

--- tdd/src/main/java/tdd/SpeakingClock.java
+++ tdd/src/main/java/tdd/SpeakingClock.java
@@ -6,8 +6,16 @@ public class SpeakingClock {
 	
 	private final static String MIDNIGHT = "Midnight";
 	private final static String NOON = "Noon";
-
-	public String getTimeAsString(LocalTime aTime) {
+	private static LocalTime internalTime;
+	public SpeakingClock(){
+		
+	}
+	
+	public SpeakingClock(LocalTime aTime){
+		internalTime = aTime;
+	}
+	
+	private static String convertLocalTimeToString(LocalTime aTime){
 		String timeToString = null;
 		if(LocalTime.MIDNIGHT.equals(aTime)){
 			timeToString = MIDNIGHT;
@@ -18,6 +26,14 @@ public class SpeakingClock {
 			timeToString = aTime.toString();
 		}
 		return timeToString;
+	}
+
+	public static String getTimeAsString(LocalTime aTime) {
+		return convertLocalTimeToString(aTime);
+	}
+	
+	public String getTimeAsString() {
+		return convertLocalTimeToString(internalTime);
 	}
 
 }
--- tdd/src/test/java/tdd/TestSpeakingClock.java
+++ tdd/src/test/java/tdd/TestSpeakingClock.java
@@ -13,15 +13,106 @@ public class TestSpeakingClock {
 	
 	@Before
 	public void setup(){
-		myClock = new SpeakingClock();
+		//myClock = new SpeakingClock();
 	}
 
+	@Test
+	public void  testStaticMidnight(){
+		//Given
+		testTime = LocalTime.MIDNIGHT;
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("Midnight", result);
+	}
+	
+	@Test
+	public void  testStaticMidnightNumeric(){
+		//Given
+		testTime = LocalTime.of(0, 0);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("Midnight", result);
+	}
+	
+	@Test
+	public void  testStaticNoon(){
+		//Given
+		testTime = LocalTime.NOON;
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("Noon", result);
+	}
+	
+	@Test
+	public void  testStaticNoonNumeric(){
+		//Given
+		testTime = LocalTime.of(12, 0);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("Noon", result);
+	}
+	
+	@Test
+	public void  testStatic2PM(){
+		//Given
+		testTime = LocalTime.of(14, 0);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("14:00", result);
+	}
+	
+	@Test
+	public void  testStaticElevenFiftyNineAM(){
+		//Given
+		testTime = LocalTime.of(11, 59);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("11:59", result);
+	}
+	
+	@Test
+	public void  testStaticOneMinutePastTwelvePM(){
+		//Given
+		testTime = LocalTime.of(12, 01);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("12:01", result);
+	}
+	
+	@Test
+	public void  testStaticElevenFiftyNinePM(){
+		//Given
+		testTime = LocalTime.of(23, 59);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("23:59", result);
+	}
+	
+	@Test
+	public void  testStaticOneMinutePastTwelveAM(){
+		//Given
+		testTime = LocalTime.of(00, 01);
+		//When
+		String result = SpeakingClock.getTimeAsString(testTime);
+		//Then
+		assertEquals("00:01", result);
+	}
+	
 	@Test
 	public void  testMidnight(){
 		//Given
 		testTime = LocalTime.MIDNIGHT;
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("Midnight", result);
 	}
@@ -30,8 +121,9 @@ public class TestSpeakingClock {
 	public void  testMidnightNumeric(){
 		//Given
 		testTime = LocalTime.of(0, 0);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("Midnight", result);
 	}
@@ -40,8 +132,9 @@ public class TestSpeakingClock {
 	public void  testNoon(){
 		//Given
 		testTime = LocalTime.NOON;
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("Noon", result);
 	}
@@ -50,8 +143,9 @@ public class TestSpeakingClock {
 	public void  testNoonNumeric(){
 		//Given
 		testTime = LocalTime.of(12, 0);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("Noon", result);
 	}
@@ -60,8 +154,9 @@ public class TestSpeakingClock {
 	public void  test2PM(){
 		//Given
 		testTime = LocalTime.of(14, 0);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("14:00", result);
 	}
@@ -70,8 +165,9 @@ public class TestSpeakingClock {
 	public void  testElevenFiftyNineAM(){
 		//Given
 		testTime = LocalTime.of(11, 59);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("11:59", result);
 	}
@@ -80,8 +176,9 @@ public class TestSpeakingClock {
 	public void  testOneMinutePastTwelvePM(){
 		//Given
 		testTime = LocalTime.of(12, 01);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("12:01", result);
 	}
@@ -90,8 +187,9 @@ public class TestSpeakingClock {
 	public void  testElevenFiftyNinePM(){
 		//Given
 		testTime = LocalTime.of(23, 59);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("23:59", result);
 	}
@@ -100,11 +198,11 @@ public class TestSpeakingClock {
 	public void  testOneMinutePastTwelveAM(){
 		//Given
 		testTime = LocalTime.of(00, 01);
+		myClock = new SpeakingClock(testTime);
 		//When
-		String result = myClock.getTimeAsString(testTime);
+		String result = myClock.getTimeAsString();
 		//Then
 		assertEquals("00:01", result);
 	}
-	
 	
 }
